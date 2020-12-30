a5828fef53c5122421404cd34777938c46b1c10d
Tobias Girstmair <tobias.girstmair@tirol.gv.at>
2020-12-30 16:15:31 +0100

    Put README into long_description for PyPi

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup, find_packages
 
+# read README (actually docs/index.rst):
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="serles-acme",
     version="1.0",
@@ -30,6 +36,8 @@ setup(
         passes actual issuance off to an existing PKI Certificate Authority.
         Extensible with plug-ins. Ships with an EJBCA Community backend.
     """,
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     keywords="pki ejbca acme server certbot",
     url="https://github.com/dvtirol/serles-acme",
     project_urls={
