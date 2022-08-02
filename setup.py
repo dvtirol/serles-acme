e3d18d67170b0435e9d6719b48e0bc8d87ffae75
Tobias Girstmair <tobias.girstmair@tirol.gv.at>
2022-08-02 21:27:08 +0200

    publish version 1.0.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name="serles-acme",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(),
     python_requires=">=3.6",
     scripts=["bin/serles"],
